3f7ba5c77b6df8648b558cfef2b04d425cdecb69
dantheman872 <95766593+dantheman872@users.noreply.github.com>
2025-10-22 11:39:19 +0100

    Update sketch.js

--- exercise1-2/sketch.js
+++ exercise1-2/sketch.js
@@ -1,6 +1,7 @@
-
 let sx = []
 let sy = []
+let sr = []
+let sg = []
 
 function setup() {
 
@@ -9,15 +10,16 @@ function setup() {
 
 function draw() {
 
-    background(20,40,200)
-    fill(100);
+    background(0)
 
     for(let i = 0; i < sx.length; i++){
 
-        star(sx[i],sy[i])
+        fill(sr[i], sg[i], 0)
+        star(sx[i], sy[i])
+        sy[i] += 2
+        sr[i] += -2
+        sg[i] += -2
     }
-
-    moveStar()
 }
 
 
@@ -28,25 +30,20 @@ function draw() {
  * @param {number} y 
  */
 function star(x, y) {
-    fill(255, 234, 20);
+
     noStroke();
     triangle(x, y - 50, x - 20, y, x + 20, y);
     triangle(x - 50, y - 20, x, y - 20, x, y + 10);
     triangle(x + 50, y - 20, x, y - 20, x, y + 10);
     triangle(x - 20, y - 5, x, y + 10, x - 35, y + 30);
     triangle(x, y + 10, x + 20, y - 5, x + 35, y + 30);
+
 }
 
 function mouseClicked(){
 
     sx.push(mouseX)
     sy.push(mouseY)
-}
-
-function moveStar(){
-
-    for(let i = 0; i < sy.length; i++){
-
-        sy[i] += 2
-    }
+    sr.push(255)
+    sg.push(234)
 }
